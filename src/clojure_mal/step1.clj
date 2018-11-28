ee01ace47a91eca539bc7c033ee4f0530491f901
Christopher Mark Gore <chris.gore@rate.com>
2018-11-28 16:57:18 -0600

    Breaking out the "printer"

--- src/clojure_mal/step1.clj
+++ src/clojure_mal/step1.clj
@@ -3,12 +3,15 @@
 (defn reader []
   (read-line))
 
+(defn printer [result]
+  (println "OKAY ...")
+  (println result))
+
 (defn repl-loop []
   (println "HOWDY >")
   (let [input-line (reader)]
     (when input-line
-      (println "OKAY ...")
-      (println input-line)
+      (printer input-line)
       (recur))))
 
 (defn -main [& args]
